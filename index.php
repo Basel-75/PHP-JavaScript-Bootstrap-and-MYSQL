cbd55ea9ca0f93ff68789c018ecc319f0d31a07c
nnnrakan444 <145937236+nnnrakan444@users.noreply.github.com>
2023-11-11 17:09:53 +0300

    init

--- index.php
+++ index.php
@@ -13,81 +13,63 @@ $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en" dir="rtl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="./css/bootstrap.rtl.min.css">
-    <link rel="stylesheet" href="./css/style.css">
-    <title>Document</title>
-</head>
-<body>
+<?php include_once './parts/header.php'; ?>
 
+<div class="container">
     <div class="position-relative overflow-hidden p-3 p-md-3 text-center bg-light">
-      <div>
-        <img src="images/nour-icon.jpg" alt="">
-        <h1 class="display-4 fw-normal">أربح مع نور</h1>
-        <p class="lead fw-normal">باقي على فتح التسجيل</p>
-        <h3 id="countdown"></h3>
-        <p class="lead fw-normal">للسحب على ربح نسخة مجانية من برنامج</p>
-        <a class="btn btn-outline-secondary" href="#">Coming soon</a>
-      </div>
+        <div>
+          <img src="images/nour-icon.jpg" alt="">
+          <h1 class="display-4 fw-normal">أربح مع نور</h1>
+          <p class="lead fw-normal">باقي على فتح التسجيل</p>
+          <h3 id="countdown"></h3>
+          <p class="lead fw-normal">للسحب على ربح نسخة مجانية من برنامج</p>
+          <a class="btn btn-outline-secondary" href="#">قريبا</a>
+        </div>
     </div>
 
     <div class="container">
-      <h3>للدخول للسحب اتبع ما يلي</h3>
-    <ul class="list-group list-group-flush">
-        <li class="list-group-item">تابع البث المباشر على صفحتي على فيسبوك بالتاريخ المذكور أعلاه</li>
-        <li class="list-group-item">سأقوم ببث مباشر لمدة ساعة عبارة عن أسئلة وأجوبة حرة للجميع</li>
-        <li class="list-group-item">خلال فترة الساعة سيتم فتح صفحة التسجيل هنا حيث ستقوم بتسجيل اسمك وإيميلك</li>
-        <li class="list-group-item">بنهاية البث سيتم اختيار اسم واحد من قاعدة البيانات بشكل عشوائي</li>
-        <li class="list-group-item">الرابح سيحصل على نسخة مجانية من برنامج كامتازيا</li>
-    </ul>
+        <h3>للدخول للسحب اتبع ما يلي</h3>
+      <ul class="list-group list-group-flush">
+          <li class="list-group-item">تابع البث المباشر على صفحتي على فيسبوك بالتاريخ المذكور أعلاه</li>
+          <li class="list-group-item">سأقوم ببث مباشر لمدة ساعة عبارة عن أسئلة وأجوبة حرة للجميع</li>
+          <li class="list-group-item">خلال فترة الساعة سيتم فتح صفحة التسجيل هنا حيث ستقوم بتسجيل اسمك وإيميلك</li>
+          <li class="list-group-item">بنهاية البث سيتم اختيار اسم واحد من قاعدة البيانات بشكل عشوائي</li>
+          <li class="list-group-item">الرابح سيحصل على نسخة مجانية من برنامج كامتازيا</li>
+      </ul>
     </div>
 
 
     <div class="position-relative text-center">
-            <div class="col-md-5 p-lg-5 mx-auto">
+      <div class="col-md-5 p-lg-5 mx-auto">
 
-    <form action="<?php $_SERVER['PHP_SELF']?>" method="POST">
-    <h3>الرجاء ادخال معلوماتك</h3>
+        <form action="<?php $_SERVER['PHP_SELF']?>" method="POST">
+          <h3>الرجاء ادخال معلوماتك</h3>
      
-  <div class="mb-3">
-    <label for="firstName" class="form-label">الاسم الأول</label>
-    <input type="text" name="firstName" class="form-control" id="firstName" value="<?php echo $firstName ?>" >
-    <div  class="form-text error"><?php echo $errors['firstNameError'] ?></div>
-  </div>
+        <div class="mb-3">
+          <label for="firstName" class="form-label">الاسم الأول</label>
+          <input type="text" name="firstName" class="form-control" id="firstName" value="<?php echo $firstName ?>" >
+          <div  class="form-text error"><?php echo $errors['firstNameError'] ?></div>
+        </div>
   
-  <div class="mb-3">
-    <label for="lastName" class="form-label">الاسم الأخير</label>
-    <input type="text" name="lastName" class="form-control" id="lastName" value="<?php echo $lastName ?>" >
-    <div  class="form-text error"><?php echo $errors['lastNameError']?></div>
-  </div>
+        <div class="mb-3">
+          <label for="lastName" class="form-label">الاسم الأخير</label>
+          <input type="text" name="lastName" class="form-control" id="lastName" value="<?php echo $lastName ?>" >
+          <div  class="form-text error"><?php echo $errors['lastNameError']?></div>
+        </div>
 
-  <div class="mb-3">
-    <label for="email" class="form-label">البريدالالكتروني</label>
-    <input type="text" name="email" class="form-control" id="email" value="<?php echo $email ?>" >
-    <div  class="form-text error"><?php echo $errors['emailError']?></div>
-  </div>
+        <div class="mb-3">
+          <label for="email" class="form-label">البريدالالكتروني</label>
+          <input type="text" name="email" class="form-control" id="email" value="<?php echo $email ?>" >
+          <div  class="form-text error"><?php echo $errors['emailError']?></div>
+        </div>
   
-  <button type="submit" name="submit" class="btn btn-primary">ارسال المعلومات</button>
-</form>
-</div>
-</div>
-
-
+        <button type="submit" name="submit" class="btn btn-primary">ارسال المعلومات</button>
+        </form>
+      </div>
+    </div>
 </div>
 
-<!--
-    <form action="index.php" method="POST">
-        <input type="text" name="firstName" id="firstName" placeholder="First name">
-        <input type="text" name="lastName" id="lastName" placeholder="Last name">
-        <input type="text" name="email" id="email" placeholder="Email">
-        <input type="submit" name="submit" value="send">
-    </form>
--->
+
 
 <div class="loader-con">
     <div id="loader">
@@ -98,9 +80,9 @@ $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 <!-- Button trigger modal -->
 <div class="d-grid gap-2 col-6 mx-auto my-5">
-<button id="winner" type="button" class="btn btn-primary" >
-  اختيار الرابح
-</button>
+  <button id="winner" type="button" class="btn btn-primary" >
+    اختيار الرابح
+  </button>
 </div>
 
 <!-- Modal -->
@@ -125,9 +107,4 @@ $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
   </div>
 </div>
 
-    <script src="./js/bootstrap.bundle.min.js"></script>
-    <script src="./js/loader.js"></script>
-    <script src="./js/script.js"></script>
-
-</body>
-</html>
+<?php include_once './parts/footer.php'; ?>
